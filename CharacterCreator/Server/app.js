92fe913cd82ae0f561b43072a5edcecb4ca012f7
Taylor Sampson <thsampsn@gmail.com>
2020-09-10 11:40:54 -0400

    push character create

--- CharacterCreator/Server/app.js
+++ CharacterCreator/Server/app.js
@@ -1,12 +1,16 @@
-let express =require('express');
+require('dotenv').config();
+let express = require('express');
 let app = express();
 const sequelize = require('./db');
 
+const chara = require('./Controllers/characontroller');
 
 
+sequelize.sync();
+// app.use(require('./middleware/headers'))
+app.use(express.json());
 
-
-
+app.use('/chara', chara);
 
 
 app.listen(3000, function(){
