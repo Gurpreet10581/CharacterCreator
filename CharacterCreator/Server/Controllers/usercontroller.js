b34657234a7e92a3dac293ad32f3bd59b1d02221
Kevin Curnow <kevinscurnow@gmail.com>
2020-09-10 16:01:23 -0400

    added bcrypt functionality for signup

--- CharacterCreator/Server/Controllers/usercontroller.js
+++ CharacterCreator/Server/Controllers/usercontroller.js
@@ -1,18 +1,22 @@
 const router = require('express').Router();
 const User = require('../db').import('../Models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post("/signup", (req,res) => {
     User.create({
         firstName: req.body.fName,
         lastName: req.body.lName,
         email: req.body.email,
-        password: req.body.password
+        password: bcrypt.hashSync(req.body.password, 10)
     })
     .then(
         createSuccess = (user) => {
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1d'})
             res.json({
                 user: user,
-                message: 'user created'
+                message: 'user created',
+                sessionToken: token
             })
         },
         createError = err => res.status(500).json(err)
