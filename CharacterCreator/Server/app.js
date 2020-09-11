fe92d7e50047f7b3c347800a09cbc71298299aab
Taylor Sampson <thsampsn@gmail.com>
2020-09-11 09:57:58 -0400

    characters now connected to users

--- CharacterCreator/Server/app.js
+++ CharacterCreator/Server/app.js
@@ -7,12 +7,17 @@ const chara = require('./Controllers/characontroller');
 let user = require('./Controllers/usercontroller');
 
 sequelize.sync();
+app.use(require('./Middleware/headers'))
+
 
 app.use(express.json());
 app.use('/user', user);
+
+app.use(require('./Middleware/validateSession'))
 app.use('/chara', chara);
 
-// app.use(require('./middleware/headers'))
+
+
 
 app.listen(3000, function(){
     console.log('App is listing on port 3000');
